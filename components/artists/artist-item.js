36f68b07e46d8eb23563b7192a32b801e96a0f24
AngelSiete <andraco660@gmail.com>
2024-09-24 14:08:28 +0200

    added socials to artists page

--- components/artists/artist-item.js
+++ components/artists/artist-item.js
@@ -3,6 +3,11 @@ export default function ArtistItem({ artist }) {
         <div>
             <img src={artist.image} alt={artist.name} />
             <h3>{artist.name}</h3>
+            <div>
+                {artist.socials.spotify && <a href={artist.socials.spotify} target="_blank">Spotify</a>}
+                {artist.socials.instagram && <a href={artist.socials.instagram} target="_blank">Instagram</a>}
+                {artist.socials.facebook && <a href={artist.socials.facebook} target="_blank">Facebook</a>}
+            </div>
         </div>
     )
 }
